35b6f3cd680c8bae1cb865655b6c3bdaae2f40d2
abdullahgm1 <agm.musalami@gmail.com>
2025-05-19 12:05:26 +0300

    Update

--- ros2_agent/setup.py
+++ ros2_agent/setup.py
@@ -12,12 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Add config files to the installation
-        ('share/' + package_name + '/config', glob('config/*.yaml')),
-        # Add launch files to the installation
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+
     ],
-    install_requires=['setuptools', 'rosa'],  # Added 'rosa' dependency
+    install_requires=['setuptools', 'rosa'],  
     zip_safe=True,
     maintainer='AbdullagGN1',
     maintainer_email='agm.musalami@gmail.com',
